3fafbe88fd6cab8d31b8446181504bd7e31a6a8e
Ryan-Kalinowski <ryankalinowski1@gmail.com>
2013-02-03 05:15:35 -0500

    Basic functionality is working.  Modeled after the example

--- src/main/java/Fuzzer.java
+++ src/main/java/Fuzzer.java
@@ -7,36 +7,38 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-
 public class Fuzzer {
 
-
-	private WebClient webClient;
-	
-	public Fuzzer(WebClient webClient) {
-		this.webClient = webClient;
-	}
-	
-	public void fuzz(String siteURL) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
-		discoverAttackSurface(siteURL);
-		
-		
-	}
-	
-	private void discoverAttackSurface(String siteURL) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
-		HtmlPage page = (HtmlPage) webClient.getPage(siteURL);
-		List<HtmlAnchor> links = page.getAnchors();
-		for (HtmlAnchor subPage : links) {
-			System.out.println(subPage.asText());
-		}
-		
-	}
-
-	public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+	public static void main(String[] args)
+			throws FailingHttpStatusCodeException, MalformedURLException,
+			IOException {
 		WebClient webClient = new WebClient();
 		webClient.setJavaScriptEnabled(true);
 		Fuzzer fuzzer = new Fuzzer(webClient);
 		fuzzer.fuzz("http://127.0.0.1:8080/bodgeit/");
 	}
-	
+
+	private WebClient webClient;
+
+	public Fuzzer(WebClient webClient) {
+		this.webClient = webClient;
+	}
+
+	public void fuzz(String siteURL) throws FailingHttpStatusCodeException,
+			MalformedURLException, IOException {
+		discoverAttackSurface(siteURL);
+
+	}
+
+	private void discoverAttackSurface(String siteURL)
+			throws FailingHttpStatusCodeException, MalformedURLException,
+			IOException {
+		HtmlPage page = (HtmlPage) webClient.getPage(siteURL);
+		List<HtmlAnchor> links = page.getAnchors();
+		for (HtmlAnchor subPage : links) {
+			System.out.println(subPage.asText());
+		}
+
+	}
+
 }
