08ed534d2469fddf18bf20193c29d3eafc4c1c9a
Ryan-Kalinowski <ryankalinowski1@gmail.com>
2013-02-04 01:10:25 -0500

    Page.getAnchors(), in preparation for Site.discoverSite()

--- src/main/java/site/Page.java
+++ src/main/java/site/Page.java
@@ -1,19 +1,33 @@
 package site;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Page {
 	URL url;
 	HtmlPage htmlPage;
+	Map<String, List<String>> arguments;
+	
 	
 	public Page(HtmlPage htmlPage) {
 		this.htmlPage = htmlPage;
 		this.url = htmlPage.getWebResponse().getUrl();
+		this.arguments = new HashMap<String, List<String>>();
 	}
 
-
+	public void discoverInputs() {
+		
+	}
+	
+	public List<HtmlAnchor> getAnchors() {
+		List<HtmlAnchor> links = htmlPage.getAnchors();
+		return links;
+	}
 	
 	
 	
