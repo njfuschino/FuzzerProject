8502c9f79d5460c985d7b75493a40426c865a86f
Ryan-Kalinowski <ryankalinowski1@gmail.com>
2013-02-12 18:41:05 -0500

    Will try attack vectors on URL arguments

--- src/main/java/fuzzer/Fuzzer.java
+++ src/main/java/fuzzer/Fuzzer.java
@@ -3,10 +3,13 @@ package fuzzer;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import site.Page;
 import site.Site;
@@ -14,10 +17,12 @@ import site.Site;
 public class Fuzzer {
 	private WebClient webClient;
 	private List<String> sensitiveData;
+	private List<String> maliciousInputs;
 
-	public Fuzzer(WebClient webClient, String sensitiveDataFilePath) throws IOException {
+	public Fuzzer(WebClient webClient, String sensitiveDataFilePath, String maliciousInputFilePath) throws IOException {
 		this.webClient = webClient;
 		this.sensitiveData = getSensitiveData(sensitiveDataFilePath);
+		this.maliciousInputs = MaliciousInputs.getMaliciousInputs(maliciousInputFilePath);
 
 	}
 
@@ -40,20 +45,30 @@ public class Fuzzer {
 		}
 	}
 
-	public void fuzz(Site site) {
+	public void fuzz(Site site) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
 		fuzzPages(site);
 	}
 
-	private void fuzzPages(Site site) {
+	private void fuzzPages(Site site) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
 		List<Page> pages = site.getPages();
 		for (Page page : pages) {
 			fuzz(page);
 		}
 	}
 
-	private void fuzz(Page page) {
+	private void fuzz(Page page) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
 		for (String argument : page.getArguments()) {
-			
+			for (String input : maliciousInputs) {
+				String urlToTest = page.getURL().toExternalForm() + "?" + argument + "=" + input;
+				try {
+					webClient.getPage(urlToTest);
+				} catch (FailingHttpStatusCodeException e) {
+					continue;
+				} catch (IOException e) {
+					continue;
+				}
+				System.out.println("Possible vulnerability at:\t" + urlToTest);
+			}
 		}
 		
 		
